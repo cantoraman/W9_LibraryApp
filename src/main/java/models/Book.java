931ee82562da1b0f8b49caaef0df23ed27c5a18d
Can Toraman <cantoraman@gmail.com>
2018-07-18 13:56:30 +0100

    creates db connection for book class

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,8 +1,13 @@
 package models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
 public class Book {
 
-
+    int id;
     String title;
     String author;
     int pages;
@@ -15,12 +20,48 @@ public class Book {
         this.genre = genre;
     }
 
+    public Book(){}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Column(name = "title")
     public String getTitle() {
         return this.title;
     }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Column(name = "author")
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Column(name = "pages")
+    public int getPages() {
+        return pages;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    public GenreType getGenre() {
+        return genre;
+    }
+    public void setGenre(GenreType genre) {
+        this.genre = genre;
     }
 }
